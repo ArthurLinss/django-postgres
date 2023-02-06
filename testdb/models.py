96c80729cbf4a44301925ec2c9392a9122645c64
Linß, Arthur <arthur.linss@dlr.de>
2023-02-06 14:30:26 +0100

    url setup

--- testdb/models.py
+++ testdb/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Subject(models.Model):
     name = models.CharField(max_length=80)
@@ -13,6 +13,7 @@ class Subject(models.Model):
 
 class Teacher(models.Model):
     name = models.CharField(max_length=80)
+    #user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
     age = models.IntegerField()
     subject = models.ManyToManyField(Subject, default=None)
 
